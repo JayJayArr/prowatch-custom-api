79e607b3b6b8076c8d547eef4b1531e4ff33176b
JayJayArr <JayArr@outlook.de>
2024-01-18 12:30:45 +0100

    update e2e

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,21 @@
-import { AuthModule } from './auth/auth.module';
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { APP_GUARD } from '@nestjs/core';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+
+import { APP_GUARD } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { Module } from '@nestjs/common';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    ThrottlerModule.forRoot({
-      ttl: 60,
-      limit: 10
-    }),
+    ThrottlerModule.forRoot([
+      {
+        limit: 10,
+        ttl: 60
+      }
+    ]),
     AuthModule
   ],
   controllers: [AppController],
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,14 +1,16 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
+
+import { Test, TestingModule } from '@nestjs/testing';
+
 import { AppModule } from './../src/app.module';
+import { INestApplication } from '@nestjs/common';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [AppModule]
     }).compile();
 
     app = moduleFixture.createNestApplication();
@@ -19,6 +21,8 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect(
+        'This API returns DATA from Honeywell BRNO, any use other than the intended use is not allowed'
+      );
   });
 });
